a12b17f3178b78b8bf6010323c21fe4777d22559
Steven Hutchison <125940072+hutchison-s@users.noreply.github.com>
2024-11-04 16:42:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.get("/player/:id", async (req, res) => {
-  const { id } = req.params();
+  const { id } = req.params;
   fs.readFile("./filteredPlayers.json", "utf-8", (err, data) => {
     if (err) {
       return res.status(400).send({ message: "Error retrieving data" });
     }
     try {
       const players = JSON.parse(data);
-      const target = players.find((x) => x.id == id);
-      res.send(target);
+      console.log(players[0]);
+      const target = players.filter((x) => x.id == id);
+      if (target.length == 0) return res.send({});
+
+      console.log(id, target[0]);
+      res.send(target[0]);
     } catch (err) {
       console.err(err);
       return res.status(400).send({ message: "error reading player data" });
